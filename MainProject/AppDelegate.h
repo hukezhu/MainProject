24591c3c7ce6feba059dd0c216a7fd0d4a3dbabb
hukezhu <595632239@163.com>
2020-07-29 00:10:44 +0800

    Initial Commit

---
+++ MainProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by hukezhu on 2020/7/29.
+//  Copyright © 2020 hukezhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by hukezhu on 2020/7/29.
+//  Copyright © 2020 hukezhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
